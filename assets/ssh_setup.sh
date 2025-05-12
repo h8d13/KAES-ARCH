66a97947cc2a79cb8d87dba6b06947831bbf7eb7
HADEON <52324046+h8d13@users.noreply.github.com>
2025-05-12 21:40:53 +0200

    Create ssh_setup.sh

---
+++ assets/ssh_setup.sh
@@ -0,0 +1,5 @@
+#pacman -S openssh
+#ufw allow 22 && ufw limit ssh
+#systemctl enable sshd
+#systemctl start sshd
+## root login is disabled by default can be changed in /etc/ssh/sshd_config or escalate with su 
