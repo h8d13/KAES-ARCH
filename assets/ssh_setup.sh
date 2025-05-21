30cc32b4207aaafb580b2a607a9c887072651162
h8d13 <hadrien.loge@gmail.com>
2025-05-21 14:35:05 +0200

    Ohsplr 14:34 #001

--- assets/ssh_setup.sh
+++ assets/ssh_setup.sh
@@ -1,5 +1,5 @@
 #pacman -S openssh
-#ufw allow 22 && ufw limit ssh
+#ufw allow 22
 #systemctl enable sshd
 #systemctl start sshd
 ## root login is disabled by default can be changed in /etc/ssh/sshd_config or escalate with su 
