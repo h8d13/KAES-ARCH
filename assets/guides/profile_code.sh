a8590c687b05722f8b730fbacd5503af3e69a79f
HADEON <52324046+h8d13@users.noreply.github.com>
2025-09-13 12:01:04 +0200

    Update profile_code.sh

--- assets/guides/profile_code.sh
+++ assets/guides/profile_code.sh
@@ -3,6 +3,7 @@
 # download the open source binaries without telemetry from here
 #https://github.com/VSCodium/vscodium/releases
 
+#cd Downloads
 ## or use this command:
 curl -s https://api.github.com/repos/VSCodium/vscodium/releases/latest \
   | grep "browser_download_url.*VSCodium-linux-x64.*tar.gz" \
@@ -12,14 +13,19 @@ curl -s https://api.github.com/repos/VSCodium/vscodium/releases/latest \
   | cut -d '"' -f 4 \
   | wget -i -
 
-
+# this is cleaner than extracting all to local bin (i'd rather have small utility scripts in bin) 
 # extract to where you want the binaries to live (usually either /opt/codium or /.local/bin) 
-# if added to local bin you can directly use: codium $PWD
-tar -xzf VSCodium-linux-x64-*.tar.gz -C ~/opt/vscodium/ --strip-components=1
+#sudo mkdir -p ~/.local/bin/vscodium
+#sudo tar -xzf VSCodium-linux-x64-*.tar.gz -C  ~/.local/bin/vscodium --strip-components=1
+
+# if added to local bin you can directly use: /vscodium/codium $PWD
+
+# sudo mkdir -p /opt/vscodium
+#sudo tar -xzf VSCodium-linux-x64-*.tar.gz -C /opt/vscodium/ --strip-components=1
 
 # if added to /opt/codium or somewhere you wanted it you can set an alias in /.config/aliases
-# ex: alias codium='/home/hadean/Downloads/VSCodium-linux-x64-1.103.25610/codium $PWD'
-# then source your new alias: '. ./aliases'
+# ex: alias code='/opt/vscodium/codium $PWD'
+
+# then source your new alias: '. ./aliases' when insde the '.config' file.
 # this opens the binary with the current path as an argument
-# this is cleaner than extracting all to local bin (i'd rather have small utility scripts in bin) 
 # also say you wanted to modify behavior you could get the build version from the releases and modify what you need. 
